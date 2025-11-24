0da4e6b9e4543937528b22e354aef7cb3d93d224
Sulayee <lin7371610@gmail.com>
2025-11-24 09:23:42 +0800

    11240923

--- src/main/java/com/tw/shopping/repository/CategoryRepository.java
+++ src/main/java/com/tw/shopping/repository/CategoryRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tw.shopping.entity.CategoryEntity;
-import com.tw.shopping.entity.ProductEntity;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer>{
     // 用code 去找大分類
